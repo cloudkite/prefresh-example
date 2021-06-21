6d1d3904824bd2223d56c2df8095c5a72eae2c7b
Jonas Budelmann <jonas.budelmann@gmail.com>
2021-06-21 14:53:15 +1200

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -8,15 +8,4 @@ export function removeNode(node: Node | null) {
     if (node != null && node.parentNode != null) {
       node.parentNode.removeChild(node);
     }
-  }
-  
-  export function findNode(
-    parent: Node | null,
-    fn: (node: Node) => boolean
-  ): ChildNode | void {
-    if (parent == null) return;
-    for (let i = 0; i < parent.childNodes.length; i++) {
-      const node = parent.childNodes[i];
-      if (fn(node)) return node;
-    }
-  }
+}
